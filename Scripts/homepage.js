c9188fffde327fa49a6d53241f5bcd38fad03882
Nhat Luong <mluong@students.mags.school.nz>
2025-10-23 13:07:31 +1300

    Experimental js code for context menu

--- Scripts/homepage.js
+++ Scripts/homepage.js
@@ -7,3 +7,16 @@ function hideSidebar() {
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "none";
 }
+
+Browser .contextMenu .userMenu = () =>
+{
+  return {
+    "command-1": {
+      name: "Command 1",
+      callback: (event) =>
+      {
+        console .log ("Clicked on Command 1");
+      },
+    },
+  };
+};
