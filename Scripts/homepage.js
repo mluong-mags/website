fdc193087d8491e5a84bb6accaa2a373a657a0ff
Nhat Luong <mluong@students.mags.school.nz>
2025-10-23 23:24:35 +1300

    Finished carousel

--- Scripts/homepage.js
+++ Scripts/homepage.js
@@ -1,8 +1,10 @@
+/* Shows the sidebar on click*/
 function showSidebar() {
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "flex";
 }
 
+/* Hides the sidebar on click */
 function hideSidebar() {
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "none";
